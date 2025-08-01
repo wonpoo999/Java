d15cf1111b961e6e5a196110947450b0bd59c25e
Park Nohjun <wonpoo999@naver.com>
2025-08-01 10:03:26 +0900

    123

--- PARTD/src/absoluteTest/JavaWordCounter.java
+++ PARTD/src/absoluteTest/JavaWordCounter.java
@@ -10,7 +10,8 @@ public class JavaWordCounter {
     // 자바 파일에서 단어 출현 수 카운트
     public static Map<String, Integer> countWordsInJavaFile(File file) throws IOException {
         Map<String, Integer> map = new TreeMap<>();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
         String line;
         while ((line = reader.readLine()) != null) {
             String[] words = line.toLowerCase().split("[^a-zA-Z]+");
@@ -26,7 +27,7 @@ public class JavaWordCounter {
     // 결과 출력
     public static void printResult(Map<String, Integer> map) {
         map.entrySet().stream()
-            .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue())) // 많이 나온 순
-            .forEach(e -> System.out.printf("%-15s : %d\n", e.getKey(), e.getValue()));
+                .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue())) // 많이 나온 순
+                .forEach(e -> System.out.printf("%-15s : %d\n", e.getKey(), e.getValue()));
     }
 }
